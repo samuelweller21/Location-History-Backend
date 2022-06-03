0f8797085eaf933ef85379d12f0dc5405833652b
Samuel Weller <samuelweller21@hotmail.com>
2022-06-03 18:10:11 +0100

    Added caffine as main cache to avoid multiple caches

--- src/main/java/com/samuelweller/config/ApplicationConfig.java
+++ src/main/java/com/samuelweller/config/ApplicationConfig.java
@@ -1,7 +1,14 @@
 package com.samuelweller.config;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
 
 import software.amazon.awssdk.regions.Region;
 
@@ -24,6 +31,18 @@ public class ApplicationConfig {
 	
 	public void setAddress(String address) {
 		this.address = address;
+	}
+	
+	@Bean
+	public Caffeine caffeineConfig() {
+	    return Caffeine.newBuilder().expireAfterWrite(60, TimeUnit.MINUTES);
+	}
+	
+	@Bean
+	public CacheManager cacheManager(Caffeine caffeine) {
+	    CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+	    caffeineCacheManager.setCaffeine(caffeine);
+	    return caffeineCacheManager;
 	}
 
 }
